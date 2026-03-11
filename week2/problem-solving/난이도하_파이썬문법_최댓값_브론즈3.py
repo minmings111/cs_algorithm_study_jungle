934629819b6561c14870c093ad26e505853bc21e
minmings111 <minmings111@gmail.com>
2026-03-12 00:01:10 +0900

    Solve BOJ 2562 : using Python list and max()

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,14 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+input_val = [int(input()) for i in range(9)]
+
+def find_max(nums):
+    max_val = max(nums)
+    for i in range(len(nums)):
+        if max_val == nums[i]:
+            idx = i+1
+    print(max_val)
+    print(idx)
+
+find_max(input_val)
