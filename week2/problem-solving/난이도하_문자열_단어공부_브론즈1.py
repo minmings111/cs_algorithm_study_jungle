cba906d7bc40ee43e3f903eaf22b8de0d1de05ed
minmings111 <minmings111@gmail.com>
2026-03-13 01:01:56 +0900

    Solve BOJ 1157: find most frequent alphabet

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,26 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+
+input_val = input().upper()
+
+def find_max_alp(strs):
+    uniq = []
+    for i in range(len(strs)): # 문자열 길이만큼 반복
+        if strs[i] not in uniq: # 유니크 문자열 탐색
+            uniq.append(strs[i])
+    
+    count_s = 0
+    for j in uniq:
+        temp_count = strs.count(j)
+        temp_str = j
+
+        if temp_count > count_s:
+            count_s = temp_count
+            final_s = j
+        elif temp_count == count_s:
+            final_s = "?"
+        
+    print(final_s)
+
+
+find_max_alp(input_val)
