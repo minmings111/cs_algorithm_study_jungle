3c01fd99683339e57c3db29610ca378aba727f02
minmings111 <minmings111@gmail.com>
2026-03-12 23:47:55 +0900

    Solve BOJ 2675: repeat characters in string

--- week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
+++ week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
@@ -1,2 +1,18 @@
 # 정수론 - 골드바흐의 추측 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/9020
+
+def repeat(str_list):
+    n_repeat = int(str_list[0])
+    temp_str = ""
+    
+    for i in range(len(str_list[1])):
+        temp_str += ((str_list[1][i])*n_repeat)
+    
+    print(temp_str)
+
+
+n = int(input())
+for i in range(n):
+    temp = list(input().split())
+    repeat(temp)
+
