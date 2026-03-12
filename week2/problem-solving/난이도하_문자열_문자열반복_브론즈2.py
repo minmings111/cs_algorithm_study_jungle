6c489705caff4951e7cb2df07ba48322af1ea8e8
minmings111 <minmings111@gmail.com>
2026-03-12 23:51:03 +0900

    Solve BOJ 2675: repeat characters in string

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,17 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+def repeat(str_list):
+    n_repeat = int(str_list[0])
+    temp_str = ""
+    
+    for i in range(len(str_list[1])):
+        temp_str += ((str_list[1][i])*n_repeat)
+    
+    print(temp_str)
+
+
+n = int(input())
+for i in range(n):
+    temp = list(input().split())
+    repeat(temp)
