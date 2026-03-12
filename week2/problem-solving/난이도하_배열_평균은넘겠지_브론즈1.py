3079b23b7fa529dc22a3c8c7d24dbeb018551b0d
minmings111 <minmings111@gmail.com>
2026-03-12 22:32:43 +0900

    Solve BOJ 4344: calculate percentage of students above average

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,28 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+case_count = int(input())
+data_list = []
+
+for i in range(case_count):
+    temp_data = list(map(int, input().split()))
+    data_list.append(temp_data)
+
+
+def tell_the_truth(case):
+    n = case[0]
+    scores = case[1:]
+    avg = sum(scores)/n
+    count = 0
+
+    for i in range(n):
+        if scores[i] > avg:
+            count += 1
+    
+    per = round(count/n*100, 3)
+    print(f"{per}%")
+
+for i in range(0,case_count):
+    tell_the_truth(data_list[i])
+
+    
