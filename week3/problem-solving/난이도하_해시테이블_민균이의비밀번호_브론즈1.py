953c4e7779c897f9cd298e36bb0a1919be7eb9f7
minmings111 <minmings111@gmail.com>
2026-03-16 17:21:35 +0900

    Solve BOJ 9933: find password by reversed string match

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,15 +1,30 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
 
+# input_len = int(input())
+# input_val = [input() for i in range(input_len)]
+
+# def find_mid_str(input_str):
+#     for i in input_str:
+#         if i[::-1] in input_str:
+#             pw = i
+#             mid = len(pw)//2
+#             print(len(pw), pw[mid])
+#             return
+
+# find_mid_str(input_val)
+
 input_len = int(input())
 input_val = [input() for i in range(input_len)]
 
 def find_mid_str(input_str):
+    rev_dict = {}
     for i in input_str:
-        if i[::-1] in input_str:
-            pw = i
-            mid = len(pw)//2
-            print(len(pw), pw[mid])
+        if i[::-1] not in rev_dict:
+            rev_dict[i] = True
+        else:
+            mid = len(i)//2
+            print(len(i), i[mid])
             return
-
+        
 find_mid_str(input_val)
