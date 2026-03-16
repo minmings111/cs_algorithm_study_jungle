c776b29f2ca67a6868b29273ff4ff6d540b87c38
minmings111 <minmings111@gmail.com>
2026-03-16 18:29:13 +0900

    Solve BOJ 2164: Card2 using queue

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,19 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+from collections import deque
+
+input_val = int(input())
+
+def find_final_card(input_num):
+    card_queue = deque(range(1, input_num+1))
+
+    while len(card_queue) > 1:
+        card_queue.popleft()
+        temp = card_queue.popleft()
+        card_queue.append(temp)
+    
+    print(card_queue.popleft())
+    return
+
+find_final_card(input_val)
