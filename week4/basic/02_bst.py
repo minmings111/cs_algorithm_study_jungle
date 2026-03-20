fd37ce6559690c65bbc9076c7a85a4690d5e7a73
minmings111 <minmings111@gmail.com>
2026-03-20 21:38:52 +0900

    Implement BST search

--- week4/basic/02_bst.py
+++ week4/basic/02_bst.py
@@ -51,12 +51,24 @@ def search_bst(root, target):
         True/False
     """
     # TODO: root가 None이면 False 반환
-    pass
+    if root == None:
+        return False
     
     # TODO: 값을 찾으면 True 반환
+    if target == root.value:
+        return True
     ## target이 작으면 왼쪽 서브트리에서 검색
     ## target이 크면 오른쪽 서브트리에서 검색
-    pass
+
+    
+    elif target < root.value:
+        result = search_bst(root.left, target)
+
+    elif target > root.value:
+        result = search_bst(root.right, target)
+    
+    return result
+    
 
 # 테스트 케이스
 if __name__ == "__main__":
