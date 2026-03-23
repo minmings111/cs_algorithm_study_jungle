67ee75b9277201b755abb0c91ff2636618bb77bc
minmings111 <minmings111@gmail.com>
2026-03-23 20:46:53 +0900

    Solve BOJ 2606: virus using graph traversal

--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,31 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+from collections import deque
+
+n = int(input())
+
+temp = int(input())
+network = [tuple(map(int, input().split())) for i in range(temp)]
+
+graph = {i:[] for i in range(1, n+1)}
+
+for u, v in network:
+    graph[u].append(v)
+    graph[v].append(u) # 양방향!
+
+visited = []
+
+que = deque([1])
+visited.append(1)
+
+while que:
+    temp = que.pop()
+    for i in graph[temp]:
+
+        if i not in visited:
+            visited.append(i)
+            que.append(i)
+
+
+print(len(visited)-1)
+
