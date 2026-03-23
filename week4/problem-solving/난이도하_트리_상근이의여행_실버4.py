ad7a4385154eabf9ecaff0df0f52a3f7fce4bffa
minmings111 <minmings111@gmail.com>
2026-03-23 15:34:50 +0900

    Solve BOJ 9372: use tree property (n-1 edges)

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,20 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+
+import sys
+input = sys.stdin.readline
+
+nn = int(input())
+
+for i in range(nn):
+    n, m = map(int, input().split())
+
+    for j in range(m):
+        a, b = map(int, input().split())
+
+    # "모든 정점 N개"을 연결하는 최소 간선의 수는 (N - 1) => 공식!
+    print(n - 1)
+        
+    
+
+
