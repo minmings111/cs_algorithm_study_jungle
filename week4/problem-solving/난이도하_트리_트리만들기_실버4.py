69c19102020de5ee75f595f21657791b7d77ca29
minmings111 <minmings111@gmail.com>
2026-03-23 14:50:10 +0900

    Solve BOJ 14244: construct tree with given leaf count

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,17 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+n, m = map(int, input().split()) # 4, 2
+
+leaf_count = 0
+last_node = 0
+
+for i in range(1, n):
+
+    if leaf_count < m:
+        print(0, i)
+        leaf_count += 1
+        last_node = i
+    else:
+        print(last_node, i)
+        last_node = i
