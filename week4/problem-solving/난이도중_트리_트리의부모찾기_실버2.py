07c23a74835f1304386311edd8ec3dcbc3bf2cae
minmings111 <minmings111@gmail.com>
2026-03-24 21:22:01 +0900

    Solve BOJ 11725: find parent of tree using BFS

--- week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
+++ week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
@@ -1,2 +1,28 @@
 # 트리 - 트리의 부모 찾기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/11725
+from collections import deque
+
+n = int(input())
+edge = [list(map(int, input().split())) for i in range(1, n)]
+
+tree = {i:[] for i in range(1, n+1)}
+
+# connect node
+for u, v in edge:
+    tree[u].append(v)
+    tree[v].append(u)
+
+# 노드 방문 시, 자신의 위치에 부모의 노드를 기록
+visited = [0]*(n+1)
+visited[1] = 1
+que = deque([1])
+
+while que:
+    curr = que.popleft()
+    for i in tree[curr]:
+        if visited[i] == 0:
+            que.append(i)
+            visited[i] = curr
+
+for i in visited[2:]:
+    print(i)
