584ee2a82589fe9cab2d6827b04a847421f75d2f
minmings111 <minmings111@gmail.com>
2026-03-28 13:07:22 +0900

    Solve BOJ 2748: Fibonacci 2 using Bottom-up DP with Tabulation

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,14 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+n = int(input())
+
+fib = {}
+fib[0] = 0
+fib[1] = 1
+fib[2] = 1
+
+for i in range(3, n+1):
+    fib[i] = fib[i-1] + fib[i-2]
+
+print(fib[n])
