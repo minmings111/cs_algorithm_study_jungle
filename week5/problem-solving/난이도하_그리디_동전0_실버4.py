f20e03d37ca946e09c4ab131f40ef74bc67ed944
minmings111 <minmings111@gmail.com>
2026-03-28 14:21:41 +0900

    Solve BOJ 11047: coin change using Greedy algorithm with reverse iteration

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,16 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+import sys
+
+n, k = map(int, input().split())
+coins = [int(sys.stdin.readline()) for i in range(n)]
+
+seleced = 0
+for i in coins[::-1]:
+    temp = k//i
+    if temp > 0:
+        seleced += temp
+        k = k%i
+
+print(seleced)
