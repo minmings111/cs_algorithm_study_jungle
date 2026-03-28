09d35e04b694c000e9447dbf2e44b78a716aaea9
minmings111 <minmings111@gmail.com>
2026-03-28 17:35:30 +0900

    Solve BOJ 1541: minimize expression using Greedy algorithm with string splitting

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,14 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+
+import sys
+strs = sys.stdin.readline().split("-")
+
+minimum = 0
+for i in range(len(strs)):
+    if i == 0:
+        minimum += sum(map(int,strs[i].split("+")))
+    else:
+        minimum -= sum(map(int,strs[i].split("+")))
+
+print(minimum)
