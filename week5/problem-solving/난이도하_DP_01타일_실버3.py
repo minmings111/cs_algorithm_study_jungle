211be3393ac0c6e3c0726a9f0a4915ba2f3d9b24
minmings111 <minmings111@gmail.com>
2026-03-28 17:37:06 +0900

    Solve BOJ 1904: 01 Tile using Bottom-up DP with space optimization

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,11 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+
+import sys
+n = int(sys.stdin.readline())
+
+bineary = [0,1,2]
+
+for i in range(3, n+1):
+    bineary[i%3] = (bineary[(i-1)%3] + bineary[(i-2)%3]) % 15746
+print(bineary[n%3])
